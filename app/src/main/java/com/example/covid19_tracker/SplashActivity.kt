297895915ff245f0d94c195c78918bbaafff4efe
Apoorv Srivastava <apoorvsrivastava.4123@gmail.com>
2021-05-30 16:49:06 +0530

    add splash activity

---
+++ app/src/main/java/com/example/covid19_tracker/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.covid19_tracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+    }
+}
